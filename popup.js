0d0efddf7b3c648599e2c9ff1ca724a7d1181843
iletai <iletai@hotmail.com>
2018-12-30 17:39:51 +0700

    ! Create tablink

---
+++ popup.js
@@ -0,0 +1,51 @@
+document.getElementById('homeTab').addEventListener('click', function () {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        i == 0 ? tablinks[i].style.backgroundColor = "red" : "none";
+    }
+    document.getElementById('Home').style.display = "block";
+});
+
+document.getElementById('aboutTab').addEventListener('click', function () {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        i == 3 ? tablinks[i].style.backgroundColor = "orange" : "none";
+    }
+    document.getElementById('About').style.display = "block";
+});
+
+document.getElementById('contactTab').addEventListener('click', function () {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById('Contact').style.display = "block";
+});
+
+document.getElementById('newTab').addEventListener('click', function () {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById('News').style.display = "block";
+});
